7020200bcc7e61268e7b9b57d1eed9d20c034e7c
Muhammadhussein Ammari <xmha97@gmail.com>
2026-08-14 22:39:09 +0330

    Update background animation

--- js/animation.js
+++ js/animation.js
@@ -1,165 +1,211 @@
-// Modified from
-// https://github.com/pakastin/nodegarden/blob/master/scripts/index.js
-
-var pixelRatio = window.devicePixelRatio
-var wWidth
-var wHeight
-var wArea
-
-var nodes = new Array(Math.sqrt(wArea) / 10 | 0)
-
-var canvas = document.createElement('canvas')
-var ctx = canvas.getContext('2d')
-var $container = document.getElementById('container')
-
-if (pixelRatio !== 1) {
-  // if retina screen, scale canvas
-  canvas.style.transform = 'scale(' + 1 / pixelRatio + ')'
-  canvas.style.transformOrigin = '0 0'
-}
-canvas.id = 'nodegarden'
-
-$container.appendChild(canvas)
-
-init()
-render()
-
-window.addEventListener('resize', init)
-
-function init () {
-  wWidth = window.innerWidth * pixelRatio
-  wHeight = window.innerHeight * pixelRatio
-  wArea = wWidth * wHeight;
-
-  // calculate nodes needed
-  nodes.length = Math.sqrt(wArea) / 12 | 0
-
-  // set canvas size
-  canvas.width = wWidth
-  canvas.height = wHeight
-  // create nodes
-  var i, len
-  for (i = 0, len = nodes.length; i < len; i++) {
-    if (nodes[i]) {
-      continue
-    }
-    nodes[i] = {
-      x: Math.random() * wWidth,
-      y: Math.random() * wHeight,
-      vx: Math.random() * 1 - 0.5,
-      vy: Math.random() * 1 - 0.5,
-      m: Math.random() * 1.5 + 10,
-      link: null,
-      pos: false
-    }
-  }
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function render () {
-  var distance
-  var direction
-  var force
-  var xForce, yForce
-  var xDistance, yDistance
-  var i, j, nodeA, nodeB, len
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-  // request new animationFrame
-  requestAnimationFrame(render)
+var width = window.innerWidth;
+var height = window.innerHeight;
 
-  // clear canvas
-  ctx.clearRect(0, 0, wWidth, wHeight)
-  // update links
-  for (i = 0, len = nodes.length - 1; i < len; i++) {
-    for (j = i + 1; j < len + 1; j++) {
-      nodeA = nodes[i]
-      nodeB = nodes[j]
-      xDistance = nodeB.x - nodeA.x
-      yDistance = nodeB.y - nodeA.y
+canvas.width = width;
+canvas.height = height;
 
-      // calculate distance
-      distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2))
-
-      if (distance < nodeA.m / 2 + nodeB.m / 2) {
-        // collision: remove smaller or equal
-        if (nodeA.m <= nodeB.m) {
-          nodeA.x = Math.random() * wWidth
-          nodeA.y = Math.random() * wHeight
-          nodeA.vx = Math.random() * 1 - 0.5
-          nodeA.vy = Math.random() * 1 - 0.5
-          nodeA.m = Math.random() * 1.5 + 1
-        }
-
-        if (nodeB.m <= nodeA.m) {
-          nodeB.x = Math.random() * wWidth
-          nodeB.y = Math.random() * wHeight
-          nodeB.vx = Math.random() * 1 - 0.5
-          nodeB.vy = Math.random() * 1 - 0.5
-          nodeB.m = Math.random() * 1.5 + 1
-        }
-        continue
-      }
-
-      if (distance > 200) {
-        // distance over 200 pixels - ignore gravity
-        continue
-      }
-
-      // calculate gravity direction
-      direction = {
-        x: xDistance / distance,
-        y: yDistance / distance
-      }
-
-      // calculate gravity force
-      force = (10 * nodeA.m * nodeB.m) / Math.pow(distance, 2)
-
-      if (force > 0.025) {
-        // cap force to a maximum value of 0.025
-        force = 0.025
-      }
-
-      // draw gravity lines
-      ctx.beginPath()
-      ctx.strokeStyle = 'rgba(28,190,255,' + force * 40 + ')'
-      ctx.moveTo(nodeA.x, nodeA.y)
-      ctx.lineTo(nodeB.x, nodeB.y)
-      ctx.stroke()
-
-      xForce = force * direction.x
-      yForce = force * direction.y
-
-      // calculate new velocity after gravity
-      var limiter = 0.1;
-     if (nodeA.pos !== nodeB.pos) {
-        nodeA.vx -= xForce * limiter;
-        nodeA.vy -= yForce * limiter;
-
-        nodeB.vx += xForce * limiter;
-        nodeB.vy += yForce * limiter;
-      } else {
-        nodeA.vx += xForce * limiter;
-        nodeA.vy += yForce * limiter;
-
-        nodeB.vx -= xForce * limiter;
-        nodeB.vy -= yForce * limiter;
-      }
-    }
-  }
-  // update nodes
-  for (i = 0, len = nodes.length; i < len; i++) {
-    ctx.beginPath()
-    ctx.arc(nodes[i].x, nodes[i].y, nodes[i].m, 0, 2 * Math.PI)
-    ctx.fill()
-
-    nodes[i].x += nodes[i].vx
-    nodes[i].y += nodes[i].vy
-
-    if (nodes[i].x > wWidth + 25 || nodes[i].x < -25 || nodes[i].y > wHeight + 25 || nodes[i].y < -25) {
-      // if node over screen limits - reset to a init position
-      nodes[i].x = Math.random() * wWidth
-      nodes[i].y = Math.random() * wHeight
-      nodes[i].vx = Math.random() * 1 - 0.5
-      nodes[i].vy = Math.random() * 1 - 0.5
-    }
-  }
+function clearCanvas() {
+	const color = getComputedStyle(document.documentElement)
+		.getPropertyValue("--background-color")
+		.trim();
+	ctx.fillStyle = color;
+	ctx.fillRect(0, 0, width, height);
 }
+
+clearCanvas();
+
+var t = 0; //counter variable
+var colors = ['#4f91f9', '#a7f94f', '#f94f4f', '#f9f74f', '#8930ff', '#fc4edf', '#ff9c51']; //color array for random color setting
+var nodeSize = 2.5; //node size (radius)
+var speed = 2; //base speed (added to each node random speed)
+var nodeAmount = 100; //node amount, the more the slower
+var nodes = []; //node array
+var lineWidth = 1; //node connection line width in pixels
+var nodePositionArray = []; //node position array for drawing the lines
+var drawLineThreshold = 150; //the threshold for drawing the lines, the more the slower
+
+//generate x random of nodes with random position and push them to the array
+for (var i = 0; i < nodeAmount; i++) {
+	var node = new Node(randomInt(0, width), randomInt(0, height), i);
+	nodes.push(node);
+}
+
+//circle function (draws circle given x, y and radius)
+function circle(x, y, radius) {
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, 2 * Math.PI);
+	ctx.closePath();
+}
+
+//line function (draws line given a set of two points(as x1,y1,x2,y2))
+function line(x1, y1, x2, y2) {
+	ctx.lineWidth = lineWidth;
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+}
+
+//calculate the distance between two points
+function distance(x1, x2, y1, y2) {
+	var xDist = x2 - x1;
+	var yDist = y2 - y1;
+	return Math.sqrt(xDist * xDist + yDist * yDist);
+}
+
+//range a numbers given a value and two ranges
+function map_range(value, low1, high1, low2, high2) {
+	return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+//check if n is even
+function isEven(n) {
+	return n == parseFloat(n) ? !(n % 2) : void 0;
+}
+
+//convert from hex to rgb
+function hexToRgb(hex) {
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	} : null;
+}
+
+//set them nodes up! (for initializing the nodePositionArray)
+for (var i = 0; i < nodes.length; i++) {
+	nodes[i].move();
+	nodes[i].draw();
+}
+
+//loop function
+function loop() {
+	//reset canvas
+	clearCanvas();
+
+	//draw the lines
+	drawLines();
+
+	//move each node and draw it
+	for (var i = 0; i < nodes.length; i++) {
+		nodes[i].move();
+		nodes[i].draw();
+	}
+
+	//repeat loop
+	requestAnimationFrame(loop);
+
+	//increase counter
+	t++;
+}
+
+//run the loop
+loop();
+
+//node constructor function
+function Node(x, y, id) {
+	this.x = x;
+	this.y = y;
+	this.id = id;
+
+	//if the node id is even, start moving them in the opposite direction
+	if (isEven(id)) {
+		this.speedX = randomInt(1, 100) / 100 + speed;
+		this.speedY = randomInt(1, 100) / 100 + speed;
+	} else {
+		this.speedX = randomInt(1, 100) / 100 - speed;
+		this.speedY = randomInt(1, 100) / 100 - speed;
+	}
+
+	this.size = nodeSize;
+
+	//set random color from array
+	this.color = colors[Math.floor(Math.random() * colors.length)];
+
+	this.move = function () {
+		this.y += this.speedY;
+		this.x += this.speedX;
+
+		//make them bounce
+		if (this.y < 1 || this.y > height - nodeSize * 2) {
+			this.speedY = -this.speedY;
+		}
+		if (this.x < 1 || this.x > width - nodeSize * 2) {
+			this.speedX = -this.speedX;
+		}
+
+		//push the position and color to the array for drawing the lines
+		nodePositionArray[this.id] = [this.x, this.y, this.color];
+	};
+
+	//draw the nodes
+	this.draw = function () {
+		ctx.fillStyle = this.color;
+		circle(this.x, this.y, nodeSize);
+		ctx.fill();
+	};
+}
+
+function drawLines() {
+	for (var i = 0; i < nodePositionArray.length - 1; i++) {
+
+		//get the origin point
+		var x1 = nodePositionArray[i][0];
+		var y1 = nodePositionArray[i][1];
+
+		//this sub-loop is made to avoid drawing the nodes twice, which leads to consume more cpu and spoils the opacity effect
+		for (var j = 0; j < nodePositionArray.length - (i + 1); j++) {
+
+			//get the destination point
+			var x2 = nodePositionArray[j + i + 1][0];
+			var y2 = nodePositionArray[j + i + 1][1];
+
+			//calculate distance between the origin and target points
+			var dist = distance(x1, x2, y1, y2);
+
+			//if distance is greater than the threshold, draw the lines
+			if (dist < drawLineThreshold) {
+				var finalOpacity = map_range(dist, 0, drawLineThreshold, 1, 0);
+				var rgbValues = hexToRgb(nodePositionArray[i][2]);
+				var color = 'rgba(' + rgbValues.r + ',' + rgbValues.g + ',' + rgbValues.b + ',' + finalOpacity + ')';
+
+				ctx.strokeStyle = color;
+				line(x1, y1, x2, y2);
+			}
+		}
+	}
+}
+
+//resize event listener
+window.addEventListener('resize', function () {
+	width = window.innerWidth;
+	height = window.innerHeight;
+
+	canvas.width = width;
+	canvas.height = height;
+
+	clearCanvas();
+
+	//reset the nodes
+	nodes = [];
+
+	//generate nodes again
+	for (var i = 0; i < nodeAmount; i++) {
+		var node = new Node(randomInt(0, width), randomInt(0, height), i);
+		nodes.push(node);
+	}
+
+	//set them up
+	for (var i = 0; i < nodes.length; i++) {
+		nodes[i].move();
+		nodes[i].draw();
+	}
+});
